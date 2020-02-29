7ff879d0eb7c73d72f907fb70117ff5ca374772f
Antoine T-S <anto291100@gmail.com>
2020-02-29 18:01:41 -0500

    commit for merge

--- Assets/Enemies/Spawner.cs
+++ Assets/Enemies/Spawner.cs
@@ -36,7 +36,7 @@ public class Spawner : MonoBehaviour
                     int spawnNumber = Random.Range(0, spawnPoints.Count);
                     GameObject enemy = Instantiate(Resources.Load<GameObject>("Enemy"), new Vector3(spawnPoints[spawnNumber].position.x, spawnPoints[spawnNumber].position.y, spawnPoints[spawnNumber].position.z), Quaternion.identity);
                     //GameObject enemy = Instantiate((GameObject)Resources.Load("Enemy"), new Vector3(35 * Mathf.Cos(angle * Mathf.Deg2Rad), 25 * Mathf.Sin(angle * Mathf.Deg2Rad), transform.position.z), Quaternion.identity);
-                    enemy.GetComponent<EnemyMovement>().speed = (float)difficulty / 3 + 1;
+                    enemy.GetComponent<EnemyMovement>().speed = (float)difficulty / 6 + 1;
                     enemy.GetComponent<EnemyAI>().Hp *= difficulty / 5 + 1;
                 }
                 timeSinceLastSpawn = 0;
