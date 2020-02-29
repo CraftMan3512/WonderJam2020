7f431e09bef5fc15fab9368d3130630f4bbb485d
Mattys <mattysgervais@gmail.com>
2020-02-29 01:10:55 -0500

    Gants fonctionne
    Heading home

--- Assets/Guns/Gun.cs
+++ Assets/Guns/Gun.cs
@@ -10,18 +10,49 @@ public class Gun : MonoBehaviour
     public float rpm = 120;
     public int dmgPerBullet = 1;
 
-    public string name;
     public GameObject bullet;
     private ArrayList bullets;
     public float barrelLenght;
     public float gunLenght;
+    private bool rightP=true;
+
+    private GameObject RightPunch;
+    private GameObject LeftPunch;
+    private float originalPos;
 
     public void Shoot(float angle)
     {
-        Debug.Log("Shot");
-        GameObject tempBullet = Instantiate(bullet,
-            new Vector3(transform.position.x+barrelLenght*Mathf.Cos((angle)*Mathf.Deg2Rad), transform.position.y+barrelLenght*Mathf.Sin((angle)*Mathf.Deg2Rad), transform.position.z),
+        if (!fist)
+        {
+            GameObject tempBullet = Instantiate(bullet,
+            new Vector3(transform.position.x + barrelLenght * Mathf.Cos((angle) * Mathf.Deg2Rad),
+                transform.position.y + barrelLenght * Mathf.Sin((angle) * Mathf.Deg2Rad), transform.position.z),
             transform.rotation);
         tempBullet.GetComponent<BulletScript>().damage = dmgPerBullet;
+        }
+        else
+        {
+            GameObject currHand;
+            if (rightP)
+                currHand = RightPunch;
+            else
+                currHand = LeftPunch;
+            currHand.GetComponent<GlovesScript>().Shoot();
+            if (rightP)
+                rightP = false;
+            else
+                rightP = true;
+        }
+    }
+
+    public void Start()
+    {
+        if (fist)
+        {
+            RightPunch = transform.GetChild(1).gameObject;
+            LeftPunch = transform.GetChild(0).gameObject;
+            originalPos = transform.GetChild(0).localPosition.y;
+        }
+        
     }
 }
