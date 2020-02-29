d71d31a75ad0b5578eb497f7d89ab49952aae48c
Antoine T-S <anto291100@gmail.com>
2020-02-29 17:16:12 -0500

    mini fix spawner range

--- Assets/Enemies/Spawner.cs
+++ Assets/Enemies/Spawner.cs
@@ -12,7 +12,7 @@ public class Spawner : MonoBehaviour
     {
         difficulty = 1;
         float angle = Random.Range(0, 360);
-        GameObject enemy = Instantiate((GameObject)Resources.Load("Enemy"), new Vector3(10 * Mathf.Cos(angle * Mathf.Deg2Rad), 10 * Mathf.Sin(angle * Mathf.Deg2Rad), transform.position.z), Quaternion.identity);
+        GameObject enemy = Instantiate((GameObject)Resources.Load("Enemy"), new Vector3(35 * Mathf.Cos(angle * Mathf.Deg2Rad), 25 * Mathf.Sin(angle * Mathf.Deg2Rad), transform.position.z), Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -23,7 +23,7 @@ public class Spawner : MonoBehaviour
             for(int i = 0; i <  1; i++) //je garde la boucle si on veut changer comment le scaling marche :)
             {
                 float angle = Random.Range(0, 360);
-                GameObject enemy = Instantiate((GameObject)Resources.Load("Enemy"), new Vector3(10 * Mathf.Cos(angle * Mathf.Deg2Rad), 10 * Mathf.Sin(angle * Mathf.Deg2Rad), transform.position.z), Quaternion.identity);
+                GameObject enemy = Instantiate((GameObject)Resources.Load("Enemy"), new Vector3(35 * Mathf.Cos(angle * Mathf.Deg2Rad), 25 * Mathf.Sin(angle * Mathf.Deg2Rad), transform.position.z), Quaternion.identity);
             }
             timeSinceLastSpawn = 0;
         }
