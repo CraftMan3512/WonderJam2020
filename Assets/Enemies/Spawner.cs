ae6bbbea0f35cc1b0a5d523626a311e7f545e15a
Memoh <CraftMan3512@gmail.com>
2020-02-29 18:00:40 -0500

    fix spawner cuz wtf

--- Assets/Enemies/Spawner.cs
+++ Assets/Enemies/Spawner.cs
@@ -21,7 +21,7 @@ public class Spawner : MonoBehaviour
             }
         difficulty = 1;
         float angle = Random.Range(0, 360);
-        GameObject enemy = Instantiate((GameObject)Resources.Load("Enemy"), new Vector3(35 * Mathf.Cos(angle * Mathf.Deg2Rad), 25 * Mathf.Sin(angle * Mathf.Deg2Rad), transform.position.z), Quaternion.identity);
+        //GameObject enemy = Instantiate((GameObject)Resources.Load("Enemy"), new Vector3(35 * Mathf.Cos(angle * Mathf.Deg2Rad), 25 * Mathf.Sin(angle * Mathf.Deg2Rad), transform.position.z), Quaternion.identity);
     }
 
     // Update is called once per frame
