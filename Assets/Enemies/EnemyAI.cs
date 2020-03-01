072fd9bd008110562b50057d8232f61d79465852
Memoh <CraftMan3512@gmail.com>
2020-02-29 22:07:43 -0500

    Damage Scaling

--- Assets/Enemies/EnemyAI.cs
+++ Assets/Enemies/EnemyAI.cs
@@ -8,7 +8,7 @@ public class EnemyAI : MonoBehaviour
   
     private int hp;
     private int maxHp;
-    private int damage;
+    public int damage;
     private GameObject target;
     private float attackCooldown;
     private int scorePoints;
@@ -27,7 +27,7 @@ public class EnemyAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     { 
-        damage = 2;
+        //damage = 2;
        // maxHp = 10;
        // hp = maxHp;
         scorePoints = 10;
